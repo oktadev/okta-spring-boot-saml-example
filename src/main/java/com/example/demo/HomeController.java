4ef9f35becf19c8b938c59dc86645418b916dea9
Matt Raible <matt.raible@okta.com>
2022-08-03 14:35:46 -0600

    Spring Boot 3 + SAML Example

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(@AuthenticationPrincipal Saml2AuthenticatedPrincipal principal, Model model) {
+        model.addAttribute("name", principal.getName());
+        model.addAttribute("emailAddress", principal.getFirstAttribute("email"));
+        model.addAttribute("userAttributes", principal.getAttributes());
+        return "home";
+    }
+
+}
